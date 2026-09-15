fee422e01200b6b527ed291e74b7ddfd67073e6c
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2026-09-15 14:55:01 -0300

    [console-chat] ApplicationRunner and welcome message.

--- console-chat/src/main/java/ai/rodolfomendes/consolechat/ConsoleChatApplication.java
+++ console-chat/src/main/java/ai/rodolfomendes/consolechat/ConsoleChatApplication.java
@@ -1,7 +1,9 @@
 package ai.rodolfomendes.consolechat;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConsoleChatApplication {
@@ -10,4 +12,10 @@ public class ConsoleChatApplication {
 		SpringApplication.run(ConsoleChatApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner init() {
+		return args -> {
+			IO.println("Welcome to the Console Chat Application!");
+		};
+	}
 }
