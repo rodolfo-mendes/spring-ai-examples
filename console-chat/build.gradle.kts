3c8b5aace11621763b1ab42f02cdb90566d398e5
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2026-09-15 14:53:18 -0300

    [console-chat] Project initialization

---
+++ console-chat/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.springframework.boot.gradle.tasks.run.BootRun
+
+plugins {
+	java
+	id("org.springframework.boot") version "4.1.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ai.rodolfomendes"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(26)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+// 1. Silences warnings when running via ./gradlew bootRun
+tasks.withType<BootRun> {
+	jvmArgs = listOf("--enable-native-access=ALL-UNNAMED")
+}
+
+// 2. Silences warnings when running the built java -jar command
+tasks.withType<BootJar> {
+	manifest {
+		attributes["Enable-Native-Access"] = "ALL-UNNAMED"
+	}
+}
---
+++ console-chat/src/main/java/ai/rodolfomendes/consolechat/ConsoleChatApplication.java
@@ -0,0 +1,13 @@
+package ai.rodolfomendes.consolechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleChatApplication.class, args);
+	}
+
+}
